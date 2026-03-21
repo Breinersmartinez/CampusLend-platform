62097af3a8f5227761c9372532a8a0747716ae75
Breynersmartinez <breynersmartinezmunoz@gmail.com>
2026-03-21 16:03:00 -0500

    Se estan actualizando los diagramas y documentacion con relacion a las entidades y atributos actuales

--- back-end/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ back-end/src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -1,13 +1,18 @@
-package com.example.demo.exeptions;
+package com.example.demo.exception;
 
+import com.example.demo.dto.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.io.IOException;
 
