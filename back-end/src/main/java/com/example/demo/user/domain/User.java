e59f7ef9c29698e72c8723d1fd0cfa4b9da8625c
Breiner Saul Martinez Muñoz <153401516+Breinersmartinez@users.noreply.github.com>
2026-04-29 01:04:47 +0000

    Se agregaron + atributos del usuario

--- back-end/src/main/java/com/example/demo/user/domain/User.java
+++ back-end/src/main/java/com/example/demo/user/domain/User.java
@@ -11,14 +11,35 @@ import java.util.UUID;
 @Data
 @Builder
 public class User {
-
+    
+    @Id
     private UUID id;
+
+    @Column (name = "card_id")
+    private String idCard;
+
+    @Column (name = "firs_name")
     private String firstname;
+
+    @Column (name = "last_name")
     private String lastName;
+
+    @Column (name = "email")
+    private String email;
+
+    @Column (name = "password_hash")
     private String password;
+
+    @Column (name = "created_at")
+    private Date created_at;
+
+    @Column (name = "update_at")
+    private Date update_at;
+    
     @ManyToOne(cascade = CascadeType.REMOVE)
     @JoinColumn(name = "role_id", referencedColumnName = "id", nullable = false)
     private Role role;
+
 
     // UserDetails methods and other getters/setters
 }
